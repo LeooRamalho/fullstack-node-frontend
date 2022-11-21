70129f33835d2b24bca0490ba2625dfd63d4b0d6
LeooRamalho <leo.ramalho003@gmail.com>
2022-11-21 03:43:20 -0300

    Variaveis de ambiente

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 app.use(express.static('build'))
-app.listen(PORT, () => console.log('Servidor Front em execução!'))
+app.listen(PORT, () => console.log(`Servidor Front em execução! ${PORT}`))
